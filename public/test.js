de936cff4f9bb1915e343a0397cea8b49b033a0f
mikeehman <nk17@rice.edu>
2015-09-26 20:23:49 -0500

    working on feasibility more

--- public/test.js
+++ public/test.js
@@ -12,8 +12,10 @@ QUnit.test("Driver-Drivee Feability Test", function(assert) {
   willie.isDriver = true;
   willie.location = "south";
   willie.hc = "saigon";
+  willie.goTime = 1;
+  willie.backTime = 1;
 
-  people.add(willie);
+  people.push(willie);
 
   var tony = new Person();
   tony.id = 1;
@@ -22,10 +24,152 @@ QUnit.test("Driver-Drivee Feability Test", function(assert) {
   tony.isDriver = true;
   tony.location = "south";
   tony.hc = "palu";
+  tony.goTime = 1;
+  tony.backTime = 1;
 
-  people.add(tony);
+  people.push(tony);
 
-  
+  assert.ok(willie!=tony);
+  assert.ok(people.length==2);
+
+  var nam = new Person();
+  nam.id = 2;
+  nam.name = "Nam Hee Gordon Kim";
+  nam.isVisitor = false;
+  nam.isDriver = false;
+  nam.location = "south";
+  nam.hc = "saigon";
+  nam.goTime = 1;
+  nam.backTime = 1;
+
+  people.push(nam);
+
+  var driverForNam = pickBestDriver(nam,[willie,tony],1);
+  console.log(driverForNam);
+
+  assert.ok(driverForNam==willie, driverForNam.name + " drives " + nam.name);
+
+  var sun = new Person();
+  sun.id = 3;
+  sun.name = "Sun Ji";
+  sun.isVisitor = false;
+  sun.isDriver = false;
+  sun.location = "south";
+  sun.hc = "saigon";
+  sun.goTime = 2;
+  sun.backTime = 2;
+
+  people.push(sun);
+
+  var david = new Person();
+  david.id = 4;
+  david.name = "David Yun";
+  david.isVisitor = false;
+  david.isDriver = true;
+  david.location = "oc";
+  david.hc = "saigon";
+  david.goTime = 1;
+  david.backTime = 2;
+
+  people.push(david);
+
+  var eric = new Person();
+  eric.id = 5;
+  eric.name = "Eric Lee";
+  eric.isVisitor = false;
+  eric.isDriver = false;
+  eric.location = "north";
+  eric.hc = "haishang";
+  eric.goTime = 2;
+  eric.backTime = 2;
+
+  people.push(eric);
+
+  var jp = new Person();
+  jp.id = 6;
+  jp.name = "John Paul Peng";
+  jp.isVisitor = false;
+  jp.isDriver = false;
+  jp.location = "oc";
+  jp.hc = "saigon";
+  jp.goTime = 1;
+  jp.backTime = 1;
+
+  people.push(jp);
+
+  var sean = new Person();
+  sean.id = 7;
+  sean.name = "Sean Lee";
+  sean.isVisitor = false;
+  sean.isDriver = true;
+  sean.location = "north";
+  sean.hc = "oman";
+  sean.goTime = 2;
+  sean.backTime = 2;
+
+  people.push(sean);
+
+  var michellew = new Person();
+  michellew.id = 8;
+  michellew.name = "Michelle Won";
+  michellew.isVisitor = false;
+  michellew.isDriver = false;
+  michellew.location = "south";
+  michellew.hc = "oman";
+  michellew.goTime = 2;
+  michellew.backTime = 2;
+
+  people.push(michellew);
+
+  var rosalina = new Person();
+  rosalina.id = 9;
+  rosalina.name = "Rosalina";
+  rosalina.isVisitor = true;
+  rosalina.isDriver = false;
+  rosalina.location = "oc";
+  rosalina.hc = "";
+  rosalina.goTime = 1;
+  rosalina.backTime = 2;
+
+  people.push(rosalina);
+
+  $.each(people, function(index,person){
+    if (person.goTime>person.backTime) {
+      throw "Input Error: goTime should always be less than or equal to backTime.";
+
+    }
+
+  });
+
+  var drivers = [];
+  $.each(people, function(index,person){
+    if (person.isDriver) {
+      drivers.push(person);
+    }
+  });
+
+  var driverForRosalina = pickBestDriver(rosalina,drivers,2);
+  var driverForJP = pickBestDriver(jp,drivers,1);
+  assert.ok(driverForJP==willie, driverForJP.name + " goes with " + jp.name);
+  assert.ok(driverForRosalina==david, driverForRosalina.name + " comes back with " + rosalina.name);
+
+  var group = putPeople(people);
+
+  console.log(group);
+
+  assertString = "";
+  $.each(group, function(index, g) {
+    $.each(g, function(jndex, driver){
+      assertString += driver.name + " drives the following: [";
+      $.each(driver.theirDrivees, function(gndex, drivee){
+        assertString += "\n" + drivee.name +", ";
+      });
+      assertString += "]";
+    })
+  });
+
+  assert.ok(group.length==4, assertString);
+
 
   // this.id = -1;
   // this.name = "John Doe";
--- public/work.js
+++ public/work.js
@@ -1,34 +1,46 @@
 
 
-function run () {}
+function run () {
+
+
+}
 
 function loadAllPeople () {}
 
 
-function pickBestDriver (person, drivers) {
+function pickBestDriver (person, drivers, period) {
   // apply feasibility criteria for the specific person
-
-
   var sameLocDriver = [];
   var bestDriver = {};
   var maxpoint = 0;
   var point = 0;
-  $.each(drivers,function(curDriver){
+
+  $.each(drivers,function(index,curDriver){
     point = 0;
 
-    if (curDriver.location == person.location) {
-      point += 5;
-    }
+    if ((period==1 && curDriver.goTime==person.goTime) || (period==2 && curDriver.backTime==person.backTime)) {
 
-    if (curDriver.hc == person.hc) {
-      point += 2;
-    }
+      if (person.location=="oc" && curDriver.hc == person.hc) {
+        if (curDriver.hasOC()) point += 5;
+        else point += 10;
+      }
 
-    point += (curDriver.spotsLeft+4);
 
-    if (point > maxpoint) {
-      maxpoint = point;
-      bestDriver = curDriver;
+
+      if ((curDriver.location != "oc" && person.location != "oc") && curDriver.location == person.location) {
+        point += 5;
+      }
+
+      if (curDriver.hc == person.hc) {
+        point += 2;
+      }
+
+      point += (curDriver.spotsLeft+4);
+
+      if (point > maxpoint) {
+        maxpoint = point;
+        bestDriver = curDriver;
+      }
     }
 
   });
@@ -38,36 +50,60 @@ function pickBestDriver (person, drivers) {
 
 function putPeople (people) {
   // people is a list of people
+  // this returns four groups of drivers with their respective passengers
   var drivers = [];
   var drivees = [];
 
+  var output = [];
+
   // divide people into drivers and drivees
-  $.each(people, function(person){
-    if (person.isDriver) drivers.add(person)
-    else drivees.add(person);
+  $.each(people, function(index, person){
+    if (person.isDriver) drivers.push(person)
+    else drivees.push(person);
   });
 
-  // pur drivees into drivers
-  $.each(drivees, function(drivee) {
-    var bestDriver = pickBestDriver(drivee, drivers);
-    drivee.theirDriver = bestDriver;
-    bestDriver.theirDrivees.add(drivee);
-
-    // adjust spots left
-    bestDriver.spotsLeft -= 1;
-    if (bestDriver.spotsLeft<=0) {
-      drivers.remove(bestDriver);
-    }
+  for (var i=1; i<=2; i++) {
+    // pur drivees into drivers
+    $.each(drivers, function(index, driver){
+      driver.spotsLeft = 4;
+      driver.theirDrivees = [];
+    });
 
 
-  });
+    $.each(drivees, function(index, drivee) {
+      var bestDriver = pickBestDriver(drivee, drivers, i);
+      drivee.theirDriver = bestDriver;
+      bestDriver.theirDrivees.push(drivee);
 
+      // adjust spots left
+      bestDriver.spotsLeft -= 1;
+      if (bestDriver.spotsLeft<=0) {
+        drivers.remove(bestDriver);
+      }
+    });
+    var group1 = [];
+    var group2 = [];
+    $.each(drivers, function(index, driver){
+      if (driver.goTime==1) {
+        group1.push(driver);
+      } else {
+        group2.push(driver);
+      }
+
+    });
+    output.push(group1);
+    output.push(group2);
+
+  };
+
+  // left with go1 go2 back1 back2
+  return output;
 
 
 
 }
 
-var person = function(){
+var Person = function(){
   // default personality
   this.id = -1;
   this.name = "John Doe";
@@ -77,11 +113,25 @@ var person = function(){
   this.theirDriver = {};
   this.theirDrivees = [this];
   this.spotsLeft = 4;
-  this.service = "3";
+  this.goTime = "1";
+  this.backTime = "1";
   this.hc = "saigon";
   this.location = "south";
   this.etc = "asdfghjkqweryuio";
+
+  this.guest = {};
+  this.host = {};
+
 };
+
+Person.prototype.hasOC = function() {
+  $.each(this.theirDrivees, function(index, drivee) {
+    if (drivee.location=="oc") {
+      return true;
+    }
+    return false;
+  });
+}
 
 // person.prototype.setName(name) {
 //   this.name = name;
@@ -133,13 +183,13 @@ function generatePeople (n) {
 
     r = Math.random();
     if (r<=0.1) {
-      person.hc = "haisheng";
+      person.hc = "haishang";
     }
 
     if (0.1<r<=0.2) {
       person.hc = "palu";
     }
-    people.add(person);
+    people.push(person);
   }
   return people;
 
